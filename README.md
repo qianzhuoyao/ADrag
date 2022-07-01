ce275c934c85650d7b05f13959f301881f81280c
QaeJey <1059116864@qq.com>
2022-07-01 15:24:57 +0800

    feat:README

--- README.md
+++ README.md
@@ -1,24 +1,72 @@
-# vue
+# ADrag3
 
-## Project setup
-```
-npm install
-```
+这是一个拖拽与操作框架,可以帮助你管理并控制整个拖拽操作流程中的节点创建,更新,与记录
 
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
+* ** hook 一个事件注册器,可以快速的通过id来给对应的DOM绑定拖拽中的开始,移动进行中,移动结束三个事件
+* ** load 加载整个操作过程中的组件的节点信息,有静态配置文件config,转译为有效节点信息compiler,基础信息template,实例信息node
+* ** render 一个调度渲染器，它可以有效的控制大量消耗资源或者耗时的组件渲染的时机,也可以对整个面板操作的一个记录
 
-### Compiles and minifies for production
-```
-npm run build
-```
+## process
 
-### Lints and fixes files
-```
-npm run lint
-```
+* ** config(load) -> compiler(load) -> node(load)
+* ** 借助 event(hook) 来对 所有有效的node(load 同第一行最后) 进行控制 并最终渲染在VUE的组件内
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+## example
+
+创建节点
+
+* ** 你需要显式的先声明一个渲染器并开启操作(start)
+
+```JavaScript
+//实例渲染器
+this.render = new Render();
+//构建一个模板,所有的node需要基于渲染器生成的模板来操作,start会返回一个模板的数组,该模板来源于LOAD内的compiler
+this.templates = this.render.start();
+//确保dom已经渲染上,为event准备，避免元素不存在
+setTimeout(() => {
+    //给每个模板绑定事件
+    this.templates.forEach(i => {
+        new PipeEvent()
+            //设置拖拽元素
+            .setDragElement(`drag${i.tag}`)
+            //设置来源元素
+            .setCopyElement(`origin${i.tag}`)
+            //默认隐藏拖拽元素
+            .dragElementHide()
+            //操作回调
+            .pipeEventStart({
+                //按下 pipe 为PipeEvent该实例本身
+                downCallback: (pipe) => {
+                },
+                //移动 pipe 为PipeEvent该实例本身
+                moveCallback: (pipe) => {
+                },
+                //放起 pipeOver 为PipeEvent该实例本身 e为放弃的原生事件对象
+                overCallback: (pipeOver, e) => {
+                    //放起后隐藏拖拽元素
+                    pipeOver.dragElementHide();
+                    //开始依赖模板进行创建渲染流程 createAPI 返回一个promise,resolve其节点实例本身（注：所有的操作都应基于模板节点实例本身）
+                    this.render.create(i, this.view.length + 1).then(tem => {
+                        //设置节点位置
+                        tem.setPosition({
+                            x: e.x,
+                            y: e.y
+                        })
+                        //设置节点大小
+                        tem.setSize({
+                            w: 100,
+                            h: 100
+                        })
+                        //设置节点层级
+                        tem.setZIndex(999)
+                    }).then(() => {
+                        //打个快照，方便undo
+                        this.render.shot()
+                        //更换vue组件的Array 指针，更新视图
+                        this.view = this.render.getNodes()
+                    })
+                },
+            });
+    })
+}, 0)
+```
--- src/ADrag3/load/node.js
+++ src/ADrag3/load/node.js
@@ -1,4 +1,5 @@
 import Template from "./template";
+
 const EVENT_CLICK = "click";
 const EVENT_DOWN = "down";
 const EVENT_UP = "up";
@@ -7,119 +8,146 @@ const EVENT_DRAGSTOP = "dragstop";
 const EVENT_RESIZESTOP = "resizestop";
 const EVENT_RESIZING = "resizing";
 export default class Node extends Template {
-  constructor(prop) {
-    super(prop);
-    this.originComponent = null;
-    this.dragComponent = null;
-    this.renderComponent = null;
-    this.events = {};
-    this.data = null;
-  }
-
-  setData(d) {
-    this.data = d;
-  }
-  getData() {
-    return this.data;
-  }
-  setTag(t) {
-    this.tag = t;
-  }
-  match(tag) {
-    this.matchReceiver(tag);
-  }
-  checkEvent(eventName) {
-    return [
-      EVENT_CLICK,
-      EVENT_DOWN,
-      EVENT_UP,
-      EVENT_DRAGGING,
-      EVENT_DRAGSTOP,
-      EVENT_RESIZESTOP,
-      EVENT_RESIZING,
-    ].includes(eventName);
-  }
-  createEvent(eventName, e) {
-    if (this.checkEvent(eventName)) {
-      const event = this.events[eventName] || [];
-      event.push(e);
+    constructor(prop) {
+        super(prop);
+        this.originComponent = null;
+        this.dragComponent = null;
+        this.renderComponent = null;
+        this.events = {};
+        this.data = null;
     }
-  }
-  runEvent(eventName, params) {
-    const event = this.events[eventName];
-    if (Array.isArray(event)) {
-      event.forEach((i) => {
-        typeof i === "function" && i.call(this, params);
-      });
-    }
-  }
-  runClick() {
-    this.runEvent(EVENT_CLICK);
-  }
-  runDOWN() {
-    this.runEvent(EVENT_DOWN);
-  }
-  runUp() {
-    this.runEvent(EVENT_UP);
-  }
-  runDragging() {
-    this.runEvent(EVENT_DRAGGING);
-  }
-  runDragstop() {
-    this.runEvent(EVENT_DRAGSTOP);
-  }
-  runResizing() {
-    this.runEvent(EVENT_RESIZING);
-  }
-  runResizestop() {
-    this.runEvent(EVENT_RESIZESTOP);
-  }
 
-  registryClickEvent(e) {
-    this.createEvent(EVENT_CLICK, e);
-  }
-  registryDownEvent(e) {
-    this.createEvent(EVENT_DOWN, e);
-  }
-  registrydraggingEvent(e) {
-    this.createEvent(EVENT_DRAGGING, e);
-  }
-  registryUpEvent(e) {
-    this.createEvent(EVENT_UP, e);
-  }
-  registryDragstopEvent(e) {
-    this.createEvent(EVENT_DRAGSTOP, e);
-  }
-  registryResizestopEvent(e) {
-    this.createEvent(EVENT_RESIZESTOP, e);
-  }
-  registryResizingstopEvent(e) {
-    this.createEvent(EVENT_RESIZING, e);
-  }
-  setOriginComponent(o) {
-    this.originComponent = o;
-  }
-  setDragComponent(d) {
-    this.dragComponent = d;
-  }
-  setRenderComponent(r) {
-    this.renderComponent = r;
-  }
-  setZIndex(z) {
-    this.setZ(z);
-  }
-  setSize({ w, h }) {
-    w && this.setW(w);
-    h && this.setH(h);
-  }
-  setPosition({ x, y }) {
-    x && this.setX(x);
-    y && this.setY(y);
-  }
-  hide() {
-    this.setV(false);
-  }
-  display() {
-    this.setV(true);
-  }
+    setData(d) {
+        this.data = d;
+    }
+
+    getData() {
+        return this.data;
+    }
+
+    setTag(t) {
+        this.tag = t;
+    }
+
+    match(tag) {
+        this.matchReceiver(tag);
+    }
+
+    checkEvent(eventName) {
+        return [
+            EVENT_CLICK,
+            EVENT_DOWN,
+            EVENT_UP,
+            EVENT_DRAGGING,
+            EVENT_DRAGSTOP,
+            EVENT_RESIZESTOP,
+            EVENT_RESIZING,
+        ].includes(eventName);
+    }
+
+    createEvent(eventName, e) {
+        if (this.checkEvent(eventName)) {
+            const event = this.events[eventName] || [];
+            event.push(e);
+        }
+    }
+
+    runEvent(eventName, params) {
+        const event = this.events[eventName];
+        if (Array.isArray(event)) {
+            event.forEach((i) => {
+                typeof i === "function" && i.call(this, params);
+            });
+        }
+    }
+
+    runClick() {
+        this.runEvent(EVENT_CLICK);
+    }
+
+    runDOWN() {
+        this.runEvent(EVENT_DOWN);
+    }
+
+    runUp() {
+        this.runEvent(EVENT_UP);
+    }
+
+    runDragging() {
+        this.runEvent(EVENT_DRAGGING);
+    }
+
+    runDragStop() {
+        this.runEvent(EVENT_DRAGSTOP);
+    }
+
+    runResizing() {
+        this.runEvent(EVENT_RESIZING);
+    }
+
+    runResizeStop() {
+        this.runEvent(EVENT_RESIZESTOP);
+    }
+
+    registryClickEvent(e) {
+        this.createEvent(EVENT_CLICK, e);
+    }
+
+    registryDownEvent(e) {
+        this.createEvent(EVENT_DOWN, e);
+    }
+
+    registryDraggingEvent(e) {
+        this.createEvent(EVENT_DRAGGING, e);
+    }
+
+    registryUpEvent(e) {
+        this.createEvent(EVENT_UP, e);
+    }
+
+    registryDragStopEvent(e) {
+        this.createEvent(EVENT_DRAGSTOP, e);
+    }
+
+    registryResizeStopEvent(e) {
+        this.createEvent(EVENT_RESIZESTOP, e);
+    }
+
+    registryResizingStopEvent(e) {
+        this.createEvent(EVENT_RESIZING, e);
+    }
+
+    setOriginComponent(o) {
+        this.originComponent = o;
+    }
+
+    setDragComponent(d) {
+        this.dragComponent = d;
+    }
+
+    setRenderComponent(r) {
+        this.renderComponent = r;
+    }
+
+    setZIndex(z) {
+        this.setZ(z);
+    }
+
+    setSize({w, h}) {
+        w && this.setW(w);
+        h && this.setH(h);
+    }
+
+    setPosition({x, y}) {
+        x && this.setX(x);
+        y && this.setY(y);
+    }
+
+    hide() {
+        this.setV(false);
+    }
+
+    display() {
+        this.setV(true);
+    }
 }
